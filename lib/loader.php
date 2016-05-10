162531ca5c364bcb24c75ece8fb0b841447a6363
Mark Slade <markandrewslade@gmail.com>
2016-05-10 00:07:04 -0400

    Added additional controller-load options and moved loader callback into class.

--- lib/loader.php
+++ lib/loader.php
@@ -6,8 +6,22 @@ function load_controller( $filename ) {
     Loader::load_controller( $filename );
 }
 
-function loader_callback( $matches ) {
-    return strtoupper( strlen( $matches[1] ) === 1 ? $matches[1] : substr( $matches[1], 1 ) );
+function load_admin_controller( $filename ) {
+    if ( is_admin() ) {
+        return load_controller( $filename );
+    }
+}
+
+function load_ajax_controller( $filename ) {
+    if ( defined( DOING_AJAX ) && DOING_AJAX ) {
+        return load_controller( $filename );
+    }
+}
+
+function load_frontend_controller( $filename ) {
+    if ( ! is_admin() ) {
+        return load_controller( $filename );
+    }
 }
 
 class Loader {
@@ -16,10 +30,14 @@ class Loader {
     public static function load_controller( $filename ) {
         $basedir = sprintf( '%s/controllers', PLUGIN_PATH );
         $path = sprintf( '%s/%s', $basedir, $filename );
-        $classname = substr( preg_replace_callback( '/(^.|-.)/', __NAMESPACE__ . '\loader_callback', $filename ), 0, -4 );
+        $classname = substr( preg_replace_callback( '/(^.|-.)/', array( __CLASS__, 'loader_classname_callback' ), $filename ), 0, -4 );
         $class = sprintf( '%s\Controllers\%s', __NAMESPACE__, $classname );
 
         require_once( $path );
         return self::$controllers[ $classname ] = new $class();
+    }
+    
+    protected static function loader_classname_callback( $matches ) {
+        return strtoupper( strlen( $matches[1] ) === 1 ? $matches[1] : substr( $matches[1], 1 ) );
     }
 }
