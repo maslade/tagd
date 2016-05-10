01b371f93939180ca41025a61465ebc3d21d4f58
Mark Slade <markandrewslade@gmail.com>
2016-05-09 20:42:33 -0400

    Initial commit.

---
+++ imgd.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Tagd
+Plugin URI: http://www.example.com/
+Description: Taggable image board.
+Version: 0.0.1
+Author: Sandfox
+*/
+
+namespace Tagd;
+
+require_once __DIR__ . '/controllers/'
